73041e504e8637d0241e31e4b030fa86057cfad6
Steve Larsen <slarsen-3@att.net>
2021-08-31 18:56:34 -0500

    added LogWorkoutModal

---
+++ src/components/LogWorkoutModal.tsx
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+import {
+  Button,
+  Form,
+  FormGroup,
+  Label,
+  Input,
+  Modal,
+  ModalBody,
+} from "reactstrap";
+import {v4 as uuidv4} from 'uuid';
+
+export default function LogWorkoutModal() {
+    
+}
