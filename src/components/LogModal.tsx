f2824190b646c7d77ebbcc6fadc660c9622d3f9c
Steve Larsen <slarsen-3@att.net>
2021-08-31 18:59:12 -0500

    added LogModal

---
+++ src/components/LogModal.tsx
@@ -0,0 +1,7 @@
+import React, { useSate } from "react";
+import { Button, Input, Modal, ModalBody, Table } from "reactstrap";
+
+export default function LogModal(props) {
+    const [workouts, setWorkouts] = useState([]);
+    
+}
