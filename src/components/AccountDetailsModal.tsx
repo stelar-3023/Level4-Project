14b8548a0982cb4b031b47f93895690821d10186
Steve Larsen <slarsen-3@att.net>
2021-09-01 11:47:50 -0500

    AccountDetailsModal complete

--- src/components/AccountDetailsModal.tsx
+++ src/components/AccountDetailsModal.tsx
@@ -1,21 +1,27 @@
 import React, { useState } from "react";
 import { Modal, ModalBody } from "reactstrap";
 
-export function AccountModal() {
+export function AccountModal(props: any) {
   const [isOpen, setIsOpen] = useState(false);
 
-  function toggleAccount() {
+  const toggleAccount = () => {
     setIsOpen(!isOpen);
-  }
+  };
 
   return (
     <React.Fragment>
-        <Modal id="modal-account" className="modal" centered={true} onClick={()=> toggleAccount} >
+      {props.renderAccount(toggleAccount)}
+      <Modal
+        id="modal-account"
+        className="modal"
+        centered={true}
+        toggle={toggleAccount}
+      >
         <ModalBody className="modal-content">
           <h2>Account Details</h2>
           <br />
           <div className="account-details">
-            <h4>account details here</h4>
+            <h4>{props.user.email}</h4>
           </div>
         </ModalBody>
       </Modal>
