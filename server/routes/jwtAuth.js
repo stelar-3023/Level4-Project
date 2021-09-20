1207f563da77ab08dc946d668e0cd2304e5183a6
Steve Larsen <slarsen-3@att.net>
2021-09-20 18:44:38 -0500

    Setup login route (token)

--- server/routes/jwtAuth.js
+++ server/routes/jwtAuth.js
@@ -47,4 +47,39 @@ router.post("/register", async (req, res) => {
     }
 })
 
+// login route
+router.post("/login", async (req, res) => {
+    try {
+        // 1. destructure the req.body
+
+        const {email, password} = req.body
+
+        // 2. check if use exists (if not then throw error)
+
+        const user = await pool.query("SELECT * FROM users WHERE user_email = $1", [email])
+
+        if (user.rows.length === 0) {
+            return res.status(401).json("Password or Email is incorrect.")
+        }
+
+        // 3. check if incoming password is the same as the database password
+
+        const validPassword = await bcrypt.compare(password, user.rows[0].user_password);
+
+        console.log(validPassword)
+
+        if (!validPassword) {
+            return res.status(401).json("Password or Email is incorrect.")
+        }
+
+        // 4. give them the jwt token
+
+        const token = jwtGenerator(user.rows[0].user_Id)
+        res.json({token})
+
+    } catch (err) {
+
+    }
+})
+
 module.exports = router;
