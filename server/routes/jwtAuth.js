4593b363918d0b65cf5e2093f877633e29c66fe5
Steve Larsen <slarsen-3@att.net>
2021-09-20 18:21:32 -0500

    Set up jwtauth (token) and database.sql

--- server/routes/jwtAuth.js
+++ server/routes/jwtAuth.js
@@ -1,30 +1,50 @@
-const router = require('express').Router();
-const pool = require('../db');
+const router = require('express').Router()
+const pool = require("../db")
+const bcrypt = require("bcrypt")
+const jwtGenerator = require("../utils/jwtGenerator")
 
 // registering
-router.post('/register', async (req, res) => {
-  try {
-    // 1. destructure the req.body (name, email, password)
+router.post("/register", async (req, res) => {
+    try {
 
-    const { name, email, password } = req.body;
+        // 1. destructure the req.body (name, email, password)
 
-    // 2. check if the user exists (if user exists then throw error)
+        const {name, email, password} = req.body;
 
-    const user = await pool.query('SELECT * FROM users WHERE user_email = $1', [
-      email,
-    ]);
+        // 2. check if user exists (if user exists then throw error)
 
-    res.json(user.rows);
+        const user = await pool.query("SELECT * FROM users WHERE user_email = $1", [email])
 
-    // 3. BCrypt the user password
+        if (user.rows.length !== 0) {
+            return res.status(401).send("User already exists")
+        }
 
-    // 4. enter the user inside our database
+        // res.json(user.rows)
 
-    // 5. generating our jwt token
-  } catch (err) {
-    console.error(err.message);
-    res.status(500).send('Server Error');
-  }
-});
+        // 3. Bcrypt the user password
+
+        const saltRound = 10;
+        const salt = await bcrypt.genSalt(saltRound);
+
+        const bcryptPassword = await bcrypt.hash(password, salt)
+
+
+        // 4. enter the user into the database
+
+        const newUser = await pool.query("INSERT INTO users (user_name, user_email, user_password) VALUES ($1, $2, $3) RETURNING *", [name, email, bcryptPassword])
+
+        // res.json(newUser)
+
+        // 5. generating our jwt token
+        const token = jwtGenerator(newUser.rows[0].user_Id)
+
+        res.json(token)
+
+
+    } catch (err) {
+        console.log(err.message)
+        res.status(500).send("Server Error")
+    }
+})
 
 module.exports = router;
