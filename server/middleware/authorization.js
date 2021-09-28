691cc09a64906da989481672d8076a5628280594
Steve Larsen <slarsen-3@att.net>
2021-09-28 14:58:09 -0500

    Changed placement of next();

--- server/middleware/authorization.js
+++ server/middleware/authorization.js
@@ -1,22 +1,21 @@
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
-module.exports = async (req, res, next) => {
-    try {
+module.exports = (req, res, next) => {
+    // Get token from header
+    const token = req.header('jwt_token');
 
-        const jwtToken = req.header('token');
-
-        if (!jwtToken) {
-            return res.status(403).json('Not Authorized');
-        }
-
-        const payload = jwt.verify(jwtToken, process.env.jwtSecret);
-
-        req.user = payload.user;
-
-    } catch (err) {
-        console.error(err.message);
-        return res.status(403).json('Not Authorized');
+    // Check if not token
+    if (!token) {
+        return res.status(403).json({msg: 'authorization denied'});
     }
-    next();
+    // Verify token
+    try {
+        // it is going to give the user id (user:{id: user.id})
+        const verify = jwt.verify(token, process.env.jwtSecret);
+        req.user = verify.user;
+        next();
+    } catch (err) {
+        res.status(401).json({msg: 'Token is not valid'});
+    }
 };
