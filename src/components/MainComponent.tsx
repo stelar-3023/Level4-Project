966ceee6fb31a2171a75a91022581263c9e65b32
Steve Larsen <slarsen-3@att.net>
2021-10-03 19:09:27 -0500

    check for authentication and redirect to correct page

--- src/components/MainComponent.tsx
+++ src/components/MainComponent.tsx
@@ -1,14 +1,52 @@
-import React from "react";
-import { BrowserRouter as Router } from "react-router-dom";
-import { Home } from "./HomeComponent";
-import { Login } from "./LoginComponent";
-import exercise from "../img/exercise.jpg";
+import { useState } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom';
+import { Home } from './HomeComponent';
+import { Login } from './LoginComponent';
+import exercise from '../img/exercise.jpg';
 
 export function Main() {
+  const [user, setUser] = useState(null);
+
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
   return (
     <Router>
-      {<Home />}
-      <img src={exercise} alt="exercise" className="exercise-img" />
+      <Switch>
+        <Route
+          exact
+          path='/'
+          render={(props) =>
+            !isAuthenticated ? <Login /> : <Redirect to='/home' />
+          }
+        />
+        <Route
+          exact
+          path='/home'
+          render={(props) =>
+            isAuthenticated ? <Home {...props} /> : <Redirect to='/' />
+          }
+        />
+      </Switch>
+      {/* {user ? <Home user={setUser} /> : <Login />} */}
+      <img
+        src={exercise}
+        alt='exercise'
+        className='exercise-img'
+        style={{
+          backgroundAttachment: 'fixed',
+          backgroundPosition: 'center',
+          backgroundRepeat: 'no-repeat',
+          backgroundSize: 'cover',
+          height: 'auto',
+          maxWidth: '100%',
+          opacity: '0.4',
+        }}
+      />
     </Router>
   );
 }
