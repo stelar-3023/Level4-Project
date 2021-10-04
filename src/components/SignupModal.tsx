f39a5fbce7c47003ee104e5e4de21f1c7dd7cd12
Steve Larsen <slarsen-3@att.net>
2021-10-04 17:05:50 -0500

    added onSubmitForm

--- src/components/SignupModal.tsx
+++ src/components/SignupModal.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { Fragment, useState } from 'react';
 import {
   Button,
   Form,
@@ -7,77 +7,109 @@ import {
   Input,
   Modal,
   ModalBody,
-} from "reactstrap";
+} from 'reactstrap';
 
 export function SignupModal(props: any) {
   const [isSignupOpen, setIsSignupOpen] = useState(false);
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [username, setUsername] = useState("");
-  // const [exercise, setExercise] = useState("");
-  // const [weight, setWeight] = useState(0);
-  // const [reps, setReps] = useState(0);
+  const [inputs, setInputs] = useState({
+    email: '',
+    password: '',
+    name: '',
+  });
+
+  const { email, password, name } = inputs;
 
   const toggleSignup = () => {
     setIsSignupOpen(!isSignupOpen);
   };
+
+  const handleChange = (e: any) => {
+    setInputs({ ...inputs, [e.target.name]: e.target.value });
+  };
+
+  const onSubmitForm = async (e: any) => {
+    e.preventDefault();
+    try {
+      const body = { email, password, name };
+      const response = await fetch('http://localhost:5000/register', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(body),
+      });
+
+      const parseRes = await response.json();
+      console.log(parseRes);
+    } catch (error) {
+      let errorMessage = 'Server error';
+      if (error instanceof Error) {
+        errorMessage = error.message;
+      }
+      console.error(errorMessage);
+    }
+  };
+
   return (
-    <React.Fragment>
+    <Fragment>
       {props.renderSignup(toggleSignup)}
       <Modal
-        id="modal-signup"
-        className="modal"
+        id='modal-signup'
+        className='modal'
+        isOpen={isSignupOpen}
         centered={true}
         toggle={toggleSignup}
       >
-        <ModalBody className="modal-content">
+        <ModalBody className='modal-content'>
           <h4>Sign up</h4>
           <br />
-          <Form id="signup-form">
-            <FormGroup className="input-field">
-              <Label for="signup-email">Email Address</Label>
+          <Form onSubmit={onSubmitForm} id='signup-form'>
+            <FormGroup className='input-field'>
+              <Label for='signup-email'>Email Address</Label>
               <Input
                 value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                type="email"
-                name="email"
-                data-testid="signup-email"
-                autoComplete="on"
-                id="signup-email"
-                placeholder="Enter email"
+                onChange={(e) => handleChange(e)}
+                type='email'
+                name='email'
+                data-testid='signup-email'
+                autoComplete='on'
+                id='signup-email'
+                placeholder='Enter email'
                 required
               />
             </FormGroup>
-            <FormGroup className="input-field">
-              <Label for="sign-up-password">Choose Password</Label>
+            <FormGroup className='input-field'>
+              <Label for='sign-up-password'>Choose Password</Label>
               <Input
                 value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                type="password"
-                name="password"
-                id="signup-password"
-                placeholder="password"
+                onChange={(e) => handleChange(e)}
+                type='password'
+                name='password'
+                id='signup-password'
+                placeholder='password'
                 required
               />
             </FormGroup>
-            <FormGroup className="input-field">
-              <Label for="signup-username">Choose username</Label>
+            <FormGroup className='input-field'>
+              <Label for='signup-username'>Choose username</Label>
               <Input
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
-                type="text"
-                name="username"
-                autoComplete="on"
-                id="signup-username"
-                placeholder="username"
+                value={name}
+                onChange={(e) => handleChange(e)}
+                type='text'
+                name='name'
+                autoComplete='on'
+                id='signup-username'
+                placeholder='username'
                 required
               />
               <br />
-              <Button type="submit" /*onClick={}*/ color="danger" size="sm">Submit</Button>
+              <Button type='submit' /*onClick={}*/ color='danger' size='sm'>
+                Submit
+              </Button>
             </FormGroup>
           </Form>
         </ModalBody>
       </Modal>
-    </React.Fragment>
+    </Fragment>
   );
 }
