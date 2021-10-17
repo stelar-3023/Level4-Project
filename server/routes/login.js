9c78eb01c19d56fdfea08a0534ab46c09e6a71ea
Steve Larsen <slarsen-3@att.net>
2021-10-17 12:51:21 -0500

    changed response in login.js to get name & email in Account Details modal

--- server/routes/login.js
+++ server/routes/login.js
@@ -52,13 +52,13 @@ router.post('/verify', authorization, (req, res) => {
   }
 });
 
-router.get('/profile', authorization, async (req, res) => {
+router.post('/profile', authorization, async (req, res) => {
   try {
     const user = await pool.query('SELECT * FROM users WHERE user_id = $1', [
       req.user.id,
     ]);
 
-    res.json(user.rows[0].user_id);
+    res.json(user.rows[0]);
   } catch (err) {
     console.error(err.message);
     res.status(500).send('Server error');
--- src/components/AccountDetailsModal.tsx
+++ src/components/AccountDetailsModal.tsx
@@ -1,12 +1,38 @@
-import { Fragment, useState } from 'react';
+import { Fragment, useState, useEffect } from 'react';
 import { Modal, ModalBody } from 'reactstrap';
 
 export function AccountModal(props: any) {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
   const [isOpen, setIsOpen] = useState(false);
 
   const toggleAccount = () => {
     setIsOpen(!isOpen);
   };
+
+  const accountDetails = async () => {
+    try {
+      const response = await fetch ("http://localhost:5000/profile/", {
+        method: "POST",
+        headers: { jwt_token: localStorage.token },
+      });
+      // console.log(response)
+      const parseRes = await response.json();
+      console.log(parseRes);
+      setName(parseRes.user_name); // undefined
+      setEmail(parseRes.user_email); // undefined
+    } catch (error) {
+      let errorMessage = 'Server error'
+      if (error instanceof Error) {
+        errorMessage = error.message;
+      }
+      console.error(errorMessage);
+    }
+  }
+
+  useEffect(() => {
+    accountDetails();
+  });
 
   return (
     <Fragment>
@@ -15,13 +41,15 @@ export function AccountModal(props: any) {
         id='modal-account'
         className='modal'
         centered={true}
+        isOpen={isOpen}
         toggle={toggleAccount}
       >
         <ModalBody className='modal-content'>
           <h2>Account Details</h2>
           <br />
           <div className='account-details'>
-            {/* <h4>{props.user.email}</h4> */}
+            <h4>{email}</h4>
+            <h4>{name}</h4>
           </div>
         </ModalBody>
       </Modal>
