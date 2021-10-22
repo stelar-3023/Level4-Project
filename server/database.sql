4387f3c4b520e97b7f7fea00358128268ae07f10
Steve Larsen <slarsen-3@att.net>
2021-10-22 15:09:45 -0500

    added exercises table

--- server/database.sql
+++ server/database.sql
@@ -13,3 +13,17 @@ CREATE TABLE users
 --insert test users
 INSERT INTO users (user_name, user_email, user_password)
 VALUES ('Steve', 'slarsen3023@gmail.com', 'test_password');
+
+CREATE TABLE exercises
+(
+    exercise_id uuid PRIMARY KEY DEFAULT
+    uuid_generate_v4(),
+    exercise varchar(255) NOT NULL,
+    reps integer NOT NULL,
+    created_at timestamp,
+    updated_at timestamp
+);
+
+--insert test exercises
+INSERT INTO exercises (exercise, reps)
+VALUES ('Bench Press', 10);
