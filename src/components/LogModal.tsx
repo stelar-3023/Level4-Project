db18d419cb93431d1e079847cc7c9b4e6a0ac2bc
Steve Larsen <slarsen-3@att.net>
2021-11-05 18:15:56 -0500

    added main modal & cancelUpdate

--- src/components/LogModal.tsx
+++ src/components/LogModal.tsx
@@ -3,9 +3,44 @@ import { Button, Input, Modal, ModalBody, Table } from 'reactstrap';
 
 export function LogModal(props: any) {
   const [isLogOpen, setIsLogOpen] = useState(false);
+  const [isUpdating, setIsUpdating] = useState(false);
   const [workouts, setWorkouts] = useState([]);
 
   const toggleLog = () => {
     setIsLogOpen(!isLogOpen);
   };
+
+  const cancelUpdate = () => {
+    setIsLogOpen(false);
+    setIsUpdating(false);
+  };
+
+
+
+
+  return (
+    <Fragment>
+      {props.renderLog(toggleLog)}
+      <Modal
+        id='modal-log'
+        className='modal'
+        isOpen={isLogOpen}
+        centered={true}
+        toggle={toggleLog}
+      >
+        <ModalBody>
+          <Button
+          className="cancel-log"
+          onClick={cancelUpdate}
+          size="sm"
+          color="danger"
+          >
+            &times;
+          </Button>
+          <h2>Workout</h2>
+          
+        </ModalBody>
+      </Modal>
+    </Fragment>
+  );
 }
