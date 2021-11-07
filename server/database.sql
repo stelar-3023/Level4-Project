95aec38214bd4a4f6d4ba16742efd72e40500747
Steve Larsen <slarsen-3@att.net>
2021-11-07 17:10:33 -0600

    added deleteExercise functionality, and get exercises

--- server/database.sql
+++ server/database.sql
@@ -16,14 +16,14 @@ VALUES ('Steve', 'slarsen3023@gmail.com', 'test_password');
 
 CREATE TABLE exercises
 (
-    exercise_id uuid PRIMARY KEY DEFAULT
-    uuid_generate_v4(),
+    exercise_id SERIAL PRIMARY KEY,
     exercise varchar(255) NOT NULL,
     reps integer NOT NULL,
+    weight integer NULL,
     created_at timestamp,
     updated_at timestamp
 );
 
 --insert test exercises
-INSERT INTO exercises (exercise, reps)
-VALUES ('Bench Press', 10);
+INSERT INTO exercises (exercise, reps, weight, created_at, updated_at)
+VALUES ('Bench Press', 10, 100, '2020-01-01 00:00:00', '2020-01-01 00:00:00');
--- src/components/LogModal.tsx
+++ src/components/LogModal.tsx
@@ -1,22 +1,100 @@
-import { Fragment, useState } from 'react';
-import { Button, Input, Modal, ModalBody, Table } from 'reactstrap';
+import { Fragment, useState, useEffect } from 'react';
+import { Button, Modal, ModalBody, Table } from 'reactstrap';
 
 export function LogModal(props: any) {
   const [isLogOpen, setIsLogOpen] = useState(false);
   const [isUpdating, setIsUpdating] = useState(false);
-  const [workouts, setWorkouts] = useState([]);
+  const [exercises, setExercises] = useState([]);
 
   const toggleLog = () => {
     setIsLogOpen(!isLogOpen);
   };
 
-  const cancelUpdate = () => {
+  const cancelModal = () => {
     setIsLogOpen(false);
     setIsUpdating(false);
   };
 
+  const deleteExercise = async (id: any) => {
+    try {
+      const deleteExercise = await fetch(
+        `http://localhost:5000/exercises/${id}`,
+        {
+          method: 'DELETE',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        }
+      );
+      console.log(deleteExercise);
+      setExercises(
+        exercises.filter((exercise: any) => exercise.exercise.id !== id)
+      );
+    } catch (error) {
+      let errorMessage = 'Sever error';
+      if (error instanceof Error) {
+        errorMessage = error.message;
+      }
+      console.log(errorMessage);
+    }
+  };
 
+  const getExercises = async () => {
+    try {
+      const response = await fetch('http://localhost:5000/exercises');
+      const parseRes = await response.json();
 
+      // console.log(parseRes);
+      setExercises(parseRes);
+    } catch (error) {
+      let errorMessage = 'Server error';
+      if (error instanceof Error) {
+        errorMessage = error.message;
+      }
+      console.log(errorMessage);
+    }
+  };
+
+  useEffect(() => {
+    getExercises();
+  }, []);
+  console.log(exercises);
+
+  const renderTable = () => {
+    return (
+      <Table>
+        <thead>
+          <tr>
+            <th>Exercises</th>
+            <th>Reps</th>
+          </tr>
+        </thead>
+        <tbody>
+          {exercises.map((exercise: any) => (
+            <tr key={exercise.exercise_id}>
+              <td>{exercise.exercise}</td>
+              <td>{exercise.reps}</td>
+              <td>
+                <Button type='submit' size='sm' className='log-button'>
+                  Modify
+                </Button>
+              </td>
+              <td>
+                <Button
+                  onClick={() => deleteExercise(exercise.exercise_id)}
+                  type='submit'
+                  size='sm'
+                  className='log-button'
+                >
+                  Delete
+                </Button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    );
+  };
 
   return (
     <Fragment>
@@ -30,15 +108,15 @@ export function LogModal(props: any) {
       >
         <ModalBody>
           <Button
-          className="cancel-log"
-          onClick={cancelUpdate}
-          size="sm"
-          color="danger"
+            className='cancel-log'
+            onClick={cancelModal}
+            size='sm'
+            color='danger'
           >
             &times;
           </Button>
           <h2>Workout</h2>
-          
+          {renderTable()}
         </ModalBody>
       </Modal>
     </Fragment>
