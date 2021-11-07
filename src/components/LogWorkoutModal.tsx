e5ef02bbf23b885072cc4598ebff742ee373db86
Steve Larsen <slarsen-3@att.net>
2021-11-07 17:11:44 -0600

    added functionalityto add exercise to db

--- src/components/LogWorkoutModal.tsx
+++ src/components/LogWorkoutModal.tsx
@@ -1,4 +1,4 @@
-import { Fragment, useState } from 'react';
+import { Fragment, useState, useEffect } from 'react';
 import {
   Button,
   Form,
@@ -9,17 +9,73 @@ import {
   ModalBody,
 } from 'reactstrap';
 
+
 // import { v4 as uuidv4 } from "uuid";
 
 export function WorkoutModal(props: any) {
+  const [state, setState] = useState({});
   const [isLogWorkoutOpen, setIsLogWorkoutOpen] = useState(false);
-  // const [workouts, setWorkouts] = useState([]);
-  const [exercise, setExercise] = useState('');
-  const [reps, setReps] = useState(0);
-  // const [weight, setWeight] = useState(0);
+  const [inputs, setInputs] = useState({
+    exercise: '',
+    reps: 0,
+  });
+
+  // clean the state in the unmount of the component
+  useEffect(() => {
+    resetInputs();
+    return () => {
+      setState({});
+    };
+  }, []);
+
+  const resetInputs = () => {
+    setInputs({
+      exercise: '',
+      reps: 0,
+    });
+  };
 
   const toggleWorkout = () => {
     setIsLogWorkoutOpen(!isLogWorkoutOpen);
+  };
+
+  const cancelModal = () => {
+    setIsLogWorkoutOpen(false);
+  };
+
+  const { exercise, reps } = inputs;
+
+  const handleChange = (e: any) => {
+    setInputs({ ...inputs, [e.target.name]: e.target.value });
+  };
+
+  const addExercise = async (e: any) => {
+    e.preventDefault();
+    try {
+      const body = {
+        exercise,
+        reps,
+      };
+      const response = await fetch('http://localhost:5000/exercises', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(body),
+      });
+      console.log(response);
+    } catch (error) {
+      let errorMessage = 'Server error';
+      if (error instanceof Error) {
+        errorMessage = error.message;
+      }
+      console.log(errorMessage);
+    }
+    // eslint-disable-next-line no-lone-blocks
+    {
+      toggleWorkout();
+      resetInputs();
+    }
   };
 
   return (
@@ -29,17 +85,26 @@ export function WorkoutModal(props: any) {
         id='modal-exercise'
         className='modal'
         centered={true}
+        isOpen={isLogWorkoutOpen}
         toggle={toggleWorkout}
       >
         <ModalBody>
-          <h4>Add Exercises</h4>
+          <Button
+            className='cancel-log'
+            onClick={cancelModal}
+            size='sm'
+            color='danger'
+          >
+            &times;
+          </Button>
+          <h2>Add Exercises</h2>
           <br />
-          <Form id='exercise-form'>
+          <Form onSubmit={addExercise} id='exercise-form'>
             <FormGroup className='input-field'>
               <Label for='exercise'>Exercise</Label>
               <Input
                 value={exercise}
-                onChange={(e) => setExercise(e.target.value)}
+                onChange={(e) => handleChange(e)}
                 type='text'
                 name='exercise'
                 autoComplete='off'
@@ -52,7 +117,7 @@ export function WorkoutModal(props: any) {
               <Label for='repetitions'>Reps</Label>
               <Input
                 value={reps}
-                onChange={(e) => setReps(parseInt(e.target.value))}
+                onChange={(e) => handleChange(e)}
                 type='number'
                 name='reps'
                 autoComplete='off'
@@ -60,8 +125,10 @@ export function WorkoutModal(props: any) {
                 placeholder='reps'
                 required
               />
+              <br />
               <Button
-                /* onClick={addExercise}*/ type='submit'
+                // onClick={addExercise}
+                type='submit'
                 color='danger'
                 size='sm'
               >
