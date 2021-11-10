545e58778b912cca0bac7b8cabdd9b40d305f398
Steve Larsen <slarsen-3@att.net>
2021-11-10 16:25:25 -0600

    moved the accountDetails function to the HomeComponent

--- src/components/AccountDetailsModal.tsx
+++ src/components/AccountDetailsModal.tsx
@@ -2,37 +2,11 @@ import { Fragment, useState, useEffect } from 'react';
 import { Modal, ModalBody } from 'reactstrap';
 
 export function AccountModal(props: any) {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
   const [isOpen, setIsOpen] = useState(false);
 
   const toggleAccount = () => {
     setIsOpen(!isOpen);
   };
-
-  const accountDetails = async () => {
-    try {
-      const response = await fetch ("http://localhost:5000/profile/", {
-        method: "POST",
-        headers: { jwt_token: localStorage.token },
-      });
-      // console.log(response)
-      const parseRes = await response.json();
-      console.log(parseRes);
-      setName(parseRes.user_name); // undefined
-      setEmail(parseRes.user_email); // undefined
-    } catch (error) {
-      let errorMessage = 'Server error'
-      if (error instanceof Error) {
-        errorMessage = error.message;
-      }
-      console.error(errorMessage);
-    }
-  }
-
-  useEffect(() => {
-    accountDetails();
-  });
 
   return (
     <Fragment>
@@ -48,8 +22,8 @@ export function AccountModal(props: any) {
           <h2>Account Details</h2>
           <br />
           <div className='account-details'>
-            <h4>Email: {email}</h4>
-            <h4>Name: {name}</h4>
+            <h4>Email: {props.email}</h4>
+            <h4>Name: {props.name}</h4>
           </div>
         </ModalBody>
       </Modal>
