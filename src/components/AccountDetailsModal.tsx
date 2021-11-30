95b1bd077e12a1595b3f2538b4f0ebc550c08735
Steve Larsen <slarsen-3@att.net>
2021-11-30 17:21:33 -0600

    able to access user_email & user_name from redux store

--- src/components/AccountDetailsModal.tsx
+++ src/components/AccountDetailsModal.tsx
@@ -1,12 +1,20 @@
 import { Fragment, useState } from 'react';
 import { Modal, ModalBody } from 'reactstrap';
+import { useSelector } from 'react-redux';
+import store from '../redux/store';
 
 export function AccountModal(props: any) {
+  const user = useSelector((state: any) => state.user);
   const [isOpen, setIsOpen] = useState(false);
 
   const toggleAccount = () => {
     setIsOpen(!isOpen);
   };
+
+  
+  console.log(user);
+  console.log("state: ", store.getState());
+  console.log(user.user.user_email);
 
   return (
     <Fragment>
@@ -22,8 +30,8 @@ export function AccountModal(props: any) {
           <h2>Account Details</h2>
           <br />
           <div className='account-details'>
-            <h4>Email: {props.email}</h4>
-            <h4>Name: {props.name}</h4>
+            <h4>Email: {user.user.user_email}</h4>
+            <h4>Name: {user.user.user_name}</h4>
           </div>
         </ModalBody>
       </Modal>
