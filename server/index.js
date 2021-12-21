e5d037475c424c5e1da71e3b913aa2b778e3c49a
Steve Larsen <slarsen-3@att.net>
2021-12-20 21:01:07 -0600

    index.js error

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5000;
+const path = require('path');
 const cors = require('cors');
 
 // process.env.PORT
@@ -30,7 +31,7 @@ app.use('/', require('./routes/exercise'));
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
-}))
+})
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
