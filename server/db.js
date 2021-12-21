c52e78128f0ca226a77ed3cbfee25ed2f24129ee
Steve Larsen <slarsen-3@att.net>
2021-12-21 17:58:46 -0600

    updated prodConfig in db.js

--- server/db.js
+++ server/db.js
@@ -11,13 +11,14 @@ require('dotenv').config();
 
 const devConfig = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
 
-const prodConfig = {
-  connectionString: process.env.DATABASE_URL, // heroku addon
-};
+const prodConfig = process.env.DATABASE_URL; // heroku addon
 
 const pool = new Pool({
   connectionString:
     process.env.NODE_ENV === 'production' ? prodConfig : devConfig,
+    ssl: {
+      rejectUnauthorized: false
+    }
 });
 
 module.exports = pool;
--- server/index.js
+++ server/index.js
@@ -11,9 +11,17 @@ const cors = require('cors');
 
 
 // middleware
-app.use(cors());
+app.use(express.json());
+app.use(function (req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Access-Control-Allow-Headers"
+  );
+  next();
+});
 app.use(express.json()); // req.body
-app.use(express.urlencoded({ extended: false }));
 
 if(process.env.NODE_ENV === 'production') {
   // serve static files
