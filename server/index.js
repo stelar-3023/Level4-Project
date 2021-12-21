61dbdbe8393bc1d410c3077c85698cfb7d36e711
Steve Larsen <slarsen-3@att.net>
2021-12-20 20:45:40 -0600

    add catch all to index.js

--- server/index.js
+++ server/index.js
@@ -28,6 +28,10 @@ app.use('/', require('./routes/login'));
 // exercise
 app.use('/', require('./routes/exercise'));
 
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+}))
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
